09f519cbe291ff579b84e8cbe14d94b0a2841f1b
FOR.FOSE <32724812+Apisit60070114@users.noreply.github.com>
2019-09-30 00:24:02 +0700

    update

--- main.js
+++ main.js
@@ -1,69 +1,31 @@
-const songSections = [
-    'Spent 24 hours, I need more hours with you',
-    'You spent the weekend getting even, ooh',
-    'We spent the late nights making things right between us',
-    'But now it\'s all good, babe',
-    'Roll that back wood, babe',
-    'And play me close',
-    'Cause girls like you run \'round with guys like me',
-    'Til sun down when I come through',
-    'I need a girl like you, yeah yeah',
-    'Girls like you love fun, and yeah, me too',
-    'What I want when I come through',
-    'I need a girl like you, yeah yeah',
-    'Yeah yeah yeah, yeah yeah yeah',
-    'I need a girl like you, yeah yeah',
-    'Yeah yeah yeah, yeah yeah yeah',
-    'I need a girl like you',
-    'I spent last night on the last flight to you',
-    'Took a whole day up tryna get way up, ooh',
-    'We spent the daylight tryna make things right between us',
-    'But now it\'s all good, babe',
-    'Roll that back wood, babe',
-    'And play me close',
-    'Cause girls like you run \'round with guys like me',
-    'Til sun down when I come through',
-    'I need a girl like you, yeah yeah',
-    'Girls like you love fun, and yeah, me too',
-    'What I want when I come through',
-    'I need a girl like you, yeah yeah',
-    'Yeah yeah yeah, yeah yeah yeah',
-    'I need a girl like you, yeah yeah',
-    'Yeah yeah yeah, yeah yeah yeah',
-    'I need a girl like you, yeah yeah',
-    'I need a girl like you, yeah yeah',
-    'I need a girl like you',
-    'Maybe it\'s 6:45',
-    'Maybe I\'m barely alive',
-    'Maybe you\'ve taken my shit for the last time',
-    'Maybe I know that I\'m drunk',
-    'Maybe I know you\'re the one',
-    'Maybe you\'re thinking it\'s better if you drive',
-    'Cause girls like you run \'round with guys like me',
-    'Til sun down when I come through',
-    'I need a girl like you, yeah',
-    'Cause girls like you run \'round with guys like me',
-    'Til sun down when I come through',
-    'I need a girl like you, yeah yeah',
-    'Girls like you love fun, and yeah, me too',
-    'What I want when I come through',
-    'I need a girl like you, yeah yeah',
-    'Yeah yeah yeah, yeah yeah yeah',
-    'I need a girl like you, yeah yeah',
-    'Yeah yeah yeah, yeah yeah yeah',
-    'I need a girl like you',
-];
-
 // change nowSection to 52 for test end game.
 let nowSection = 0;
 let nowLetter = 0;
 let time = 0;
 let isStart = false;
-let audio = new Audio('assets/Girls Like You.mp3');
-audio.loop = true;
-setInterval(counTime, 1000);
 
-function start() {
+function init() {
+    let songsDisplay = document.getElementById('songsDisplay');
+    for (i = 0; i < songs.length; i++) {
+        let songDisplay = document.createElement("DIV");
+        songDisplay.classList.add('song');
+        console.log(songs[i])
+        songDisplay.innerHTML = `
+        <div class="pd">
+            <p>${songs[i].name}</p>
+            <img src="assets/img/song${songs[i].id}.jpg" alt="">
+            <button class="start-btn" onclick="start(${songs[i].id})">Slect</button>
+        <div>
+            `;
+        songsDisplay.appendChild(songDisplay);
+    }
+}
+
+function start(songId) {
+    let song = songs[songId]
+    let audio = new Audio(`assets/song${song.id}.mp3`);
+    audio.loop = true;
+    setInterval(counTime, 1000);
     audio.play();
     time = 0;
     isStart = true;
@@ -82,19 +44,19 @@ function start() {
     document.getElementById('start').classList.remove('visible');
     document.getElementById('time').classList.add('visible');
     document.getElementById('title').classList.add('startTitle');
-    checkKey();
-    createSection(nowSection);
+    checkKey(song.lyrics, audio);
+    createSection(song.lyrics, nowSection);
 
 }
 
-function createSection(section) {
+function createSection(song, section) {
     let letter = null;
-    for (i = 0; i < songSections[section].length; i++) {
+    for (i = 0; i < song[section].length; i++) {
         (function (i) {
             setTimeout(function () {
                 letter = document.createElement('span');
                 letter.classList.add('letter');
-                letter.innerHTML = songSections[section][i];
+                letter.innerHTML = song[section][i];
 
                 dSection.appendChild(letter);
             }, 30 * i);
@@ -102,27 +64,28 @@ function createSection(section) {
     }
 }
 
-function checkKey() {
-    document.addEventListener('keyup', (e) => {
+function checkKey(song, audio) {
+    document.addEventListener('keypress', (e) => {
         console.log(e.key)
-        console.log(songSections[nowSection][nowLetter])
-        if (e.key === songSections[nowSection][nowLetter]) {
+        console.log(song[nowSection][nowLetter])
+        if (e.key === song[nowSection][nowLetter]) {
             document.getElementsByClassName('letter')[nowLetter].classList.add('typed');
             nowLetter++;
-            if (nowLetter >= songSections[nowSection].length) {
+            if (nowLetter >= song[nowSection].length) {
                 nowSection++;
                 dSection.innerHTML = "";
                 nowLetter = 0;
-                if (nowSection >= songSections.length) {
+                if (nowSection >= song.length) {
                     isStart = false;
                     nowSection = 0;
                     document.getElementById('time').classList.remove('visible');
                     document.getElementById('end').classList.add('visible');
                     score.innerHTML = time;
-                    document.getElementById('start').innerHTML = 'Play Again';
                     document.getElementById('start').classList.add('visible');
+                    audio.pause();
+                    bgAudio.classList.remove('visible');
                 } else {
-                    createSection(nowSection);
+                    createSection(song, nowSection);
                 }
             }
         } else {
